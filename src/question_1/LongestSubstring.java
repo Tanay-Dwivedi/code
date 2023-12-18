2458aa6ca08d0e10c0c6ac4f8610bb96fabdb46b
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-12-19 01:12:00 +0530

    Question 1 done

--- src/question_1/LongestSubstring.java
+++ src/question_1/LongestSubstring.java
@@ -1,7 +1,49 @@
 package question_1;
 
+import java.util.*;
+import java.lang.*;
+
 public class LongestSubstring {
+    // Method to find the longest substring without repetition
+    public String longestSubstringWithoutRepetition(String s) {
+        int n = s.length();
+
+        // for checking the current substring without repetition
+        int start = 0;
+        int end = 0;
+
+        // for checking the max length
+        int maxLength = 0;
+
+        // to check the position of the current substring without repetition
+        int currentStart = 0;
+        Map<Character, Integer> charIndexMap = new HashMap<>();
+
+        while (end < n) {
+            char currentChar = s.charAt(end);
+
+            if (charIndexMap.containsKey(currentChar) && charIndexMap.get(currentChar) >= currentStart) {
+                currentStart = charIndexMap.get(currentChar) + 1;
+            }
+
+            charIndexMap.put(currentChar, end);
+
+            // to compare the lengths of the strings with the maxLength
+            if (end - currentStart > maxLength) {
+                // Update the start index and maximum length
+                start = currentStart;
+                maxLength = end - currentStart;
+            }
+
+            end++;
+        }
+
+        return s.substring(start, start + maxLength + 1);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LongestSubstring ob = new LongestSubstring();
+        String s = "Welcome to PowerRouter";
+        System.out.println(ob.longestSubstringWithoutRepetition(s));
     }
 }
--- src/question_1/readme.md
+++ src/question_1/readme.md
@@ -1,1 +1,27 @@
 ![img.png](img.png)
+
+<h2>Update- </h2>
+
+- Input - "Welcome to PowerRouter"
+- Output - "lcome to"
+
+<br>
+
+**Explanation**
+
+- I have used this code to find the longest substring within a given string without any repeated characters.
+- I begin by initializing variables to keep track of the current substring (start and end indices), the maximum length
+  of the substring without repetition (maxLength), and the starting position of the current substring without
+  repetition (currentStart).
+- To efficiently keep track of character indices, I use a HashMap (charIndexMap).
+- I use a while loop to iteratively go through each character in the input string.
+- For each character at the current end position, I check if it is repeated within the current substring. If it is, I
+  adjust the starting position (currentStart) to the next index after the repeated character.
+- I update the charIndexMap with the current character's index.
+- I compare the length of the current substring with the maximum length encountered so far (maxLength). If the current
+  substring is longer, I update the starting index (start) and maximum length (maxLength).
+- Then I move to the next character in the string by incrementing the end index.
+- The final result is the substring without repetition with the maximum length. I determine this by using the start and
+  maxLength information.
+- Basically I've applied a sliding window approach to efficiently find and update the longest substring without repeated
+  characters in the given string.
