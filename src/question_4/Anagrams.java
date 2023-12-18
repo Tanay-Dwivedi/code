ef6d91cfa0783c201e5e196f0b256e9ded61596b
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-12-19 01:42:07 +0530

    Question 4 done

--- src/question_4/Anagrams.java
+++ src/question_4/Anagrams.java
@@ -1,7 +1,35 @@
 package question_4;
 
+import java.util.Arrays;
+
 public class Anagrams {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str1 = "Welcome";
+        String str2 = "ceelmow";
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        // to check if length of strings are equal
+        if (str1.length() == str2.length()) {
+
+            char[] charArray1 = str1.toCharArray();
+            char[] charArray2 = str2.toCharArray();
+
+            Arrays.sort(charArray1);
+            Arrays.sort(charArray2);
+
+            // Checking if sorted character arrays are equal
+            boolean result = Arrays.equals(charArray1, charArray2);
+
+            if (result) {
+                System.out.println(str1 + " and " + str2 + " are anagrams.");
+            } else {
+                System.out.println(str1 + " and " + str2 + " are not an anagram.");
+            }
+        } else {
+            // Print if the lengths of the strings are not equal
+            System.out.println(str1 + " and " + str2 + " are not an anagram.");
+        }
     }
 }
--- src/question_4/readme.md
+++ src/question_4/readme.md
@@ -1,1 +1,21 @@
 ![img.png](img.png)
+
+<h2>Update- </h2>
+
+- Input - "Welcome", "ceelmow"
+- Output - "Welcome and ceelmow are anagrams."
+
+**Explanation**
+
+- I start with two strings, str1 and str2, that I want to check for being anagrams.
+- I convert both strings to lowercase using toLowerCase() to ensure a case-insensitive comparison.
+- I check if the lengths of the two strings are equal.
+    - If not, I print that the strings are not anagrams because anagrams must have the same length.
+- I convert each string into a character array (charArray1 and charArray2).
+- I sort both character arrays using Arrays.sort().
+- I check if the sorted character arrays are equal using Arrays.equals().
+    - If they are equal, the strings are anagrams; otherwise, they are not.
+- I print the result of the anagram check based on the comparison result.
+- If the lengths of the two strings are not equal, I print that they are not anagrams.
+- Basically the logic involves converting strings to lowercase, checking their lengths, sorting their characters, and
+  comparing the sorted characters to determine if the strings are anagrams or not.
